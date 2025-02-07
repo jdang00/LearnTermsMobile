4342e6b195a866bd5641e19481dffca7d3c69300
jdang00 <justindang02@gmail.com>
2025-02-07 14:05:07 -0600

    Initial Commit

---
+++ LearnTerms/LearnTermsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTermsApp.swift
+//  LearnTerms
+//
+//  Created by Justin Dang on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnTermsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnTermsTests/LearnTermsTests.swift
@@ -0,0 +1,16 @@
+//
+//  LearnTermsTests.swift
+//  LearnTermsTests
+//
+//  Created by Justin Dang on 2/7/25.
+//
+
+import Testing
+
+struct LearnTermsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
